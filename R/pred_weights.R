4ea63dba01ffc127adb897193106c6c5dfcdad75
Richard Vargas <emailrichardvargas@gmail.com>
2024-09-13 10:51:57 -0400

    cleaned up the rest of the warnings and errors noted from the check()

--- R/pred_weights.R
+++ R/pred_weights.R
@@ -1,19 +1,31 @@
 #' Calculate Predictive Weights
 #'
-#' This function calculates predictive weights by computing the inverse square sum of a correlation matrix derived from the specified variables. In 'group' analysis, it directly uses the variables for correlation. In 'prepost' analysis, it calculates the difference between two time points before correlation.
+#' This function calculates predictive weights by computing the inverse square sum of a
+#' correlation matrix derived from the specified variables. In 'group' analysis, it
+#' directly uses the variables for correlation. In 'prepost' analysis, it calculates
+#' the difference between two time points before correlation.
 #'
+#' @importFrom stats cor
 #' @param dataset A data frame containing the dataset to be analyzed.
-#' @param vars A vector of strings specifying the names of the variables to be used in the correlation analysis.
-#' @param gtvar The name of the categorical variable used to identify groups in 'prepost' type analysis.
-#' @param type The type of analysis. Valid values are 'group' for group-based correlation analysis or 'prepost' for pre-post analysis. Defaults to 'group'.
-#' @param id The variable in the dataset that uniquely identifies subjects in a 'prepost' analysis. This should not be `NULL` if `type` is 'prepost'.
+#' @param vars A vector of strings specifying the names of the variables to be used
+#' in the correlation analysis.
+#' @param gtvar The name of the categorical variable used to identify groups in
+#' 'prepost' type analysis.
+#' @param type The type of analysis. Valid values are 'group' for group-based
+#' correlation analysis or 'prepost' for pre-post analysis. Defaults to 'group'.
+#' @param id The variable in the dataset that uniquely identifies subjects in a
+#' 'prepost' analysis. This should not be `NULL` if `type` is 'prepost'.
 #' @param pre Specifies the baseline time point for 'prepost' analysis.
 #' @param post Specifies the follow-up time point for 'prepost' analysis.
-#' @param corr_method The method of correlation. Valid options are 'pearson', 'kendall', or 'spearman'. Defaults to 'pearson'.
+#' @param corr_method The method of correlation. Valid options are 'pearson',
+#' 'kendall', or 'spearman'. Defaults to 'pearson'.
 #'
 #' @return A numeric vector of predictive weights for each variable analyzed.
 #'
-#' @details This function performs error handling to ensure appropriate input values and types. It calculates the correlation matrix for the specified variables and then computes the predictive weights as the inverse square sum of the correlation matrix.
+#' @details This function performs error handling to ensure appropriate input
+#' values and types. It calculates the correlation matrix for the specified
+#' variables and then computes the predictive weights as the inverse square
+#' sum of the correlation matrix.
 #'
 #' @examples
 #' data("group_data_example")
@@ -35,12 +47,14 @@
 #'# simple group
 #'pred_weights(dataset=group_data_example, vars=c('v1', 'v2'), gtvar='group')
 #'# simple prepost
-#'pred_weights(dataset=pre_post_data_example, vars=c('v1','v2','v3'), gtvar='time', id='ID', pre=0,post=12)
+#'pred_weights(dataset=pre_post_data_example, vars=c('v1','v2','v3'),
+#'gtvar='time', id='ID', pre=0,post=12)
 #' # simulated group
-#'pred_weights(dataset=group_cog_data, vars=grp_endpts, gtvar="group.factor", type="group",corr_method="pearson")
+#'pred_weights(dataset=group_cog_data, vars=grp_endpts, gtvar="group.factor",
+#'type="group",corr_method="pearson")
 #' # simulated prepost
-#'pred_weights(dataset=pre_post_fit, id="ID", vars=prepost_endpts, gtvar="Time", type="prepost",
-#'        pre=0, post=1, corr_method="pearson")
+#'pred_weights(dataset=pre_post_fit, id="ID", vars=prepost_endpts,
+#'gtvar="Time", type="prepost",pre=0, post=1, corr_method="pearson")
 #'
 #'
 #' @export
